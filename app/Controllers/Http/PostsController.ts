63f5d71f11b0ae31edbec0b4155951c8ca8974c5
Darsheax <darsheax.program@gmail.com>
2022-03-30 21:39:36 +0200

    Create PROVIDER for location from IP + Add `location` in CONTEXT

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,10 +1,14 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Event from "@ioc:Adonis/Core/Event";
+import {HttpContextContract} from "@ioc:Adonis/Core/HttpContext";
 
 export default class PostsController {
 
-  public async index(){
+  public async index(ctx: HttpContextContract){
+
+    console.log(ctx.location)
+
     Event.emit('new:user', { id: 1 })
     return [
       {
---
+++ contracts/context.ts
@@ -0,0 +1,8 @@
+declare module '@ioc:Adonis/Core/HttpContext' {
+
+  import { Lookup } from 'geoip-lite'
+
+  interface HttpContextContract {
+    location: Lookup | null
+  }
+}
