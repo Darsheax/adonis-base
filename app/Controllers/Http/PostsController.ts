be367b495aca2ef5d418091829bc0cb667c7e461
Darsheax <darsheax.program@gmail.com>
2022-04-02 12:11:38 +0200

    Improve bouncer + 'delete' method

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -2,24 +2,39 @@
 
 import {HttpContextContract} from "@ioc:Adonis/Core/HttpContext";
 import Post from "App/Models/Post";
+import NotFoundException from "App/Exceptions/Types/NotFoundException";
 
 export default class PostsController {
 
-  public async index({bouncer}: HttpContextContract){
+  public async index({bouncer} : HttpContextContract) {
 
     const posts = await Post.all()
-    //const userIsAllowed = await bouncer.with('PostPolicy').allows('viewPreniumList')
-    //const bouncedPosts = await Post.query().withScopes(async (scopes) => scopes.showPrenium(userIsAllowed))
 
-    return await Promise.all(posts.map(async post => await bouncer.with('PostPolicy').allows('viewPost', post))).then(array => posts.filter((_, i) => array[i]))
+    return await Promise
+      .all(posts.map(async post => await bouncer.with('PostPolicy').allows('viewPost', post)))
+      .then(array => posts.filter((_, i) => array[i]))
+
   }
 
-  public async admin(){
+  public async admin() {
     return [
       {
         message: 'bienvenu ADMIN'
       }
     ]
+  }
+
+  public async delete({bouncer, params, response} : HttpContextContract) {
+
+    try {
+      const post = await Post.findOrFail(56411)
+      return {success: true, e: post}
+
+    }catch {
+      throw new NotFoundException('You are not authorized')
+
+    }
+
   }
 
 }
--- app/Policies/PostPolicy.ts
+++ app/Policies/PostPolicy.ts
@@ -12,15 +12,11 @@ export default class PostPolicy extends BasePolicy {
     }
   }
 
-  public async viewPreniumList(user: User){
-    return [Role.PRENIUM].includes(user.role)
-  }
-
   public async viewPost(user: User, post : Post){
 
-    if(user.id !== post.userId && !post.published) return Bouncer.deny('This post is not yet published', 404)
+    if(user.id !== post.userId && !post.published) return Bouncer.deny('This post is not yet published', 403)
 
-    if(![Role.PRENIUM].includes(user.role) && post.isPrenium) return Bouncer.deny('You do not have access to this page', 404)
+    if(![Role.PRENIUM].includes(user.role) && post.isPrenium) return Bouncer.deny('You do not have access to this page', 403)
 
     return true
   }
